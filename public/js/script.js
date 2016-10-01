cc21712454c4d767a028d0bacec98a4ac70c82c7
Nacho Lopez <ncho.lopez@gmail.com>
2016-10-01 21:26:01 +0200

    Search for hacktoberfest issues.

--- public/js/script.js
+++ public/js/script.js
@@ -19,4 +19,11 @@ $(document).ready(function() {
         }
         e.preventDefault();
     });
+
+    $(".btn-searchIssues").on("click",function(){
+        $('#issues-result').html('<div class="row"><div class="large-12 columns"><h2><img src="/img/ajax-loader.gif" alt="loading" /></h2></div></div>');
+        $.get('/issues', function (html) {
+            $('#issues-result').html(html);
+        });
+    })
 });
