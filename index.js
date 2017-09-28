43fb59adc136557d14d9672150e76bbdfa153970
Ian Jenkins <ian@jenkins-web.co.uk>
2017-09-28 17:57:55 +0100

    Added 2017 edition (#97)

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
-var express = require('express');
-var GitHubApi = require("github");
-var _ = require('lodash');
-var q = require('q');
-var exphbs  = require('express-handlebars');
-var cache = require('memory-cache');
-var marked = require('marked');
+'use strict';
 
-var hbs = exphbs.create({
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const compression = require('compression');
+const flash = require('express-flash');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
+const _ = require('lodash');
+const q = require('q');
+const GitHubApi = require('github');
+
+// Load environment variables from .env file
+dotenv.load();
+
+// Controllers
+const IndexController = require('./controllers/index');
+
+const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
     helpers: {
+        ifeq: function(a, b, options) {
+            if (a === b) {
+                return options.fn(this);
+            }
+            return options.inverse(this);
+        },
+        toJSON : function(object) {
+            return JSON.stringify(object);
+        },
         exists: function (variable, options) {
             if (typeof variable !== 'undefined') {
                 return options.fn(this);
             }
         },
-
         timeago: require('helper-timeago')
     },
     extname: 'hbs'
 });
 
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-app.set('port', (process.env.PORT || 5000));
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.use('/normalize-css', express.static('bower_components/normalize-css'));
-app.use('/foundation/css', express.static('bower_components/foundation/css'));
-app.use('/foundation/js', express.static('bower_components/foundation/js'));
-app.use(express.static('public'));
-
-var github = new GitHubApi({
+const github = new GitHubApi({
     version: "3.0.0",
     debug: false,
     protocol: "https",
@@ -40,264 +52,31 @@ var github = new GitHubApi({
         "user-agent": "Hacktoberfest Checker"
     }
 });
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('port', process.env.PORT || 5000);
+app.set('q', q);
+app.set('_', _);
+app.set('github', github);
 
-if (process.env.GITHUB_TOKEN) {
-    github.authenticate({
-        type: "oauth",
-        token: process.env.GITHUB_TOKEN
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', IndexController.index);
+
+// Production error handler
+if (app.get('env') === 'production') {
+    app.use(function(err, req, res, next) {
+        console.error(err.stack);
+        res.sendStatus(err.status || 500);
     });
-} else {
-    console.log('No GITHUB_TOKEN specified, do so to increase rate limit');
 }
 
-var octoberOpenPrs = [];
-var userImage;
-var ISSUES_UPDATE_INTERVAL = 60000;
-var ISSUES_DISPLAY_LIMIT = 20;
-var clientList = [];
-
-function getPullRequests(username) {
-    var deferred,
-        options;
-
-    deferred = q.defer();
-
-    options = {
-        q: '-label:invalid+created:2016-09-30T00:00:00-12:00..2016-10-31T23:59:59-12:00+type:pr+is:public+author:' + username
-    };
-
-    github.search.issues(options, function(err, res) {
-        if (err) {
-            deferred.reject();
-            return;
-        }
-
-        userImage = null;
-
-        _.each(res.items, function(event) {
-            var repo = event.pull_request.html_url.substring(0, event.pull_request.html_url.search('/pull'));
-
-            if (userImage == null) {
-                userImage = event.user.avatar_url;
-            }
-
-            var hacktoberFestLabels = _.filter(event.labels, function(label) {
-                return label.name.toLowerCase() === 'hacktoberfest';
-            });
-
-            var returnedEvent = {
-                repo_name: repo,
-                title: event.title,
-                url: event.html_url,
-                state: event.state,
-                hasHacktoberFestLabel: hacktoberFestLabels.length > 0
-            };
-
-            octoberOpenPrs.push(returnedEvent);
-        });
-
-        deferred.resolve();
-    });
-
-    return deferred.promise;
-}
-
-var totalIssues = 0;
-var octoberOpenIssues = [];
-var tempIssues = [];
-
-function getIssues(){
-    var deferred = q.defer();
-
-    var options = {
-        q : 'type:issue+label:hacktoberfest+state:open',
-        sort : 'created',
-        order : 'desc'
-    };
-
-    tempIssues = [];
-    github.search.issues(options, function(err, res) {
-        if (err) {
-            deferred.reject();
-            return;
-        }
-
-        totalIssues = res.total_count;
-
-        var issuesLen = res.items.length;
-
-        _.forEach(res.items, function(issue, k) {
-            var issueUrl = issue.html_url;
-
-            var repo_url = issueUrl.replace(/\/(issues)\/\d+/, "");
-            var lastSlash = repo_url.lastIndexOf("/");
-            var repo = repo_url.substring(repo_url.lastIndexOf("/", lastSlash - 1) + 1);
-
-            var description = issue.body;
-            var temp_desc = description;
-            if (description.length > 500) {
-                // Check if there is a link in the description
-                // if it starts before 120 chars, we include the
-                // whole link.
-                var link_match = /\[.+\]\(.+\)/;
-                var matched = description.match(link_match);
-                var match_index = description.search(link_match);
-                if (matched && match_index < 120) {
-                    description = description.substring(0, match_index) + matched + "&hellip;";
-                } else {
-                    description = description.substring(0, 120) + "&hellip;";
-                }
-            }
-
-            var returnedIssue = {
-                repo_url: repo_url,
-                repo_name: repo,
-                title: issue.title,
-                url: issue.html_url,
-                labels: issue.labels,
-                description: marked(description),
-                created: issue.created_at,
-                avatar: issue.user.avatar_url
-            };
-
-            addRepoLanguages(returnedIssue, (k === issuesLen - 1) ? deferred : false).then(function(readyissue) {
-                tempIssues.push(readyissue);
-            });
-        });
-
-
-    });
-
-    return deferred.promise;
-}
-
-function addRepoLanguages(issue, parentPromise) {
-    var deferred = q.defer();
-
-    var repo = issue.repo_name.split("/");
-
-    github.repos.get({
-        user: repo[0],
-        repo: repo[1]
-    }, function(err, res) {
-        if (err) {
-            issue.language = false;
-        } else {
-            issue.language = res.language;
-        }
-        deferred.resolve(issue);
-        if (parentPromise) {
-            parentPromise.resolve();
-        }
-    });
-    return deferred.promise;
-
-}
-
-app.get('/', function(req, res) {
-    if (!req.query.username) {
-        return res.render('index', {issues: octoberOpenIssues, total: totalIssues});
-    }
-
-    getPullRequests(req.query.username).then(function() {
-        var length,
-            statements;
-
-        length = octoberOpenPrs.length;
-        statements = ["It's not too late to start!", "Keep going.", "Half way there.", "So close!", "Way to go!", "Now you're just showing off."];
-        if (length > 5) length = 5;
-
-        if (req.xhr) {
-          res.render('partials/prs', {prs: octoberOpenPrs, statement: statements[length], userImage: userImage});
-        } else {
-          res.render('index', {prs: octoberOpenPrs, statement: statements[length], username: req.query.username, userImage: userImage});
-        }
-
-        octoberOpenPrs = [];
-    }).catch(function() {
-        if (req.xhr) {
-            res.render('partials/error');
-        } else {
-            res.render('index', {error: true, username: req.query.username});
-        }
-
-        octoberOpenPrs = [];
-    });
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get('/issues', function (req, res) {
-    getIssues().then(function () {
-        if (req.xhr) {
-            res.render('partials/issues', {issues: octoberOpenIssues, total: totalIssues});
-        } else {
-            res.render('partials/error');
-        }
-
-        octoberOpenIssues = [];
-    }).catch(function () {
-        res.render('partials/error');
-        octoberOpenIssues = [];
-    });
-});
-
-app.get('/2016', function (req, res) {
-    res.render('2016');
-});
-
-server.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
-
-io.on('connection', function (socket) {
-    // Add new connection too client list
-    clientList.push(socket);
-
-    socket.on('disconnect', function() {
-        // remove client from the list
-        clientList.splice(clientList.indexOf(socket), 1);
-    });
-
-    socket.on('error', function() {
-        // remove client from the list
-        clientList.splice(clientList.indexOf(socket), 1);
-    });
-});
-
-function getIssuesCycle() {
-    getIssues().then(function() {
-        // Render the partial then send the html through the socket.
-        // Optimize this too only send the object.
-        // Insert the newer list of issues in front.
-        octoberOpenIssues = tempIssues.concat(octoberOpenIssues);
-        octoberOpenIssues.sort(createdAtSort);
-
-        octoberOpenIssues = octoberOpenIssues.filter(function (val, index, arr) {
-            if (index < arr.length - 1) {
-                //console.log(val.url === arr[index+1].url);
-                return val.url !== arr[index+1].url;
-            }
-            return true;
-        });
-
-        // Limit issues to be rendered
-        if (octoberOpenIssues.length > ISSUES_DISPLAY_LIMIT) {
-            octoberOpenIssues.splice(ISSUES_DISPLAY_LIMIT - 1);
-        }
-        hbs.render('./views/partials/issues.hbs', {issues: octoberOpenIssues, total: totalIssues})
-            .then(function(html) {
-                // Update all clients
-                io.sockets.emit('github-issues', {html: html});
-            });
-    }).catch(function(err) {
-        io.sockets.emit('github-error', {error: true});
-    });
-
-    setTimeout(getIssuesCycle, ISSUES_UPDATE_INTERVAL);
-}
-
-function createdAtSort(a, b) {
-    return new Date(a.created).valueOf() < new Date(b.created).valueOf() ? 1 : -1;
-}
-
-// Get initial data
-getIssuesCycle();
+module.exports = app;
---
+++ public/js/main.js
@@ -0,0 +1,117 @@
+if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = HacktoberfestChecker;
+} else {
+    $(document).ready(function() {
+        new HacktoberfestChecker().constructor();
+    });
+}
+
+function HacktoberfestChecker() {
+    // DOM nodes cache
+    this.username = $('input[name=username]');
+    this.form = $('form');
+    this.results = $('#results');
+    // some configurable error messages
+    this.errors = {
+        emptyUsername: "Username cannot be blank.",
+        API: {
+            issue: "An error occurred while fetching new issues, have you set your github token? ",
+            username: "An error occurred while fetching issues for the given username, have you set your github token? ",
+        }
+    };
+    //the path for the spinner
+    this.loader = "/img/ajax-loader.gif";
+}
+/**
+ * in the constructor we'll initialize/retrieve everything that needs to fire when we new this object up
+ */
+HacktoberfestChecker.prototype.constructor = function() {
+    this.setFocus();
+    this.bindEvents();
+};
+/**
+ * the bind events function can be extended as the app grows
+ */
+HacktoberfestChecker.prototype.bindEvents = function() {
+    this.form.on("submit", this.getUsernameIssues.bind(this));
+};
+/**
+ * Set the focus on the username field
+ */
+HacktoberfestChecker.prototype.setFocus = function() {
+    this.username.focus();
+};
+
+HacktoberfestChecker.prototype.getUsernameIssues = function(e) {
+    if (e) {
+        e.preventDefault();
+    }
+    const name = this.getName();
+
+    if (!name) {
+        this.results.html(this.makeError(this.errors.emptyUsername));
+        return;
+    }
+    this.results.html(this.makeSpinner());
+
+    $.ajax({
+        url: '/?username=' + name,
+        type: "GET",
+        success: this.usernameIssuesSuccess.bind(this),
+        //new: add error handler in case of failure during the API call
+        error: this.usernameIssuesError.bind(this),
+        always: this.updateHistory.apply(this, [name])
+    });
+
+};
+
+/**
+ * In case of success during API call, display the HTML
+ */
+HacktoberfestChecker.prototype.usernameIssuesSuccess = function(html) {
+    this.results.html(html);
+};
+/**
+ * In case of an error during API call, display an error message
+ */
+HacktoberfestChecker.prototype.usernameIssuesError = function(html) {
+    this.results.html(this.makeError(this.errors.API.username));
+};
+
+HacktoberfestChecker.prototype.getName = function() {
+    return this.username.val().trim() || false;
+};
+/**
+ * HTML Helper makeSpinner
+ * create the necessary HTML to show the spinner using fluent syntax
+ */
+HacktoberfestChecker.prototype.makeSpinner = function() {
+    return $("<div/>").addClass('loading').append(
+        $("<h2/>").append(
+            $("<img/>", {
+                src: this.loader,
+                alt: "loading"
+            })
+        )
+    );
+};
+/**
+ * HTML Helper makeSpinner
+ * create the necessary HTML to show an error message using fluent syntax
+ */
+HacktoberfestChecker.prototype.makeError = function(error) {
+    return $("<div/>").append(
+        $("<h2/>", {
+            text: error
+        })
+    );
+};
+/**
+ * History state helper, checks if the replaceState is available
+ * before updating the username in the query string.
+ */
+HacktoberfestChecker.prototype.updateHistory = function(name) {
+    if (window.history && window.history.replaceState) {
+        history.replaceState({}, name, '?username=' + name);
+    }
+};
