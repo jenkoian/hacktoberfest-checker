38c361a60366724a221b27d7c265f0e1aa671372
Alexander Arvidsson <alexander@arvidson.nu>
2017-10-03 21:16:03 +0200

    User avatar is now shown for people without PRs

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,6 @@
  * GET /
  */
 exports.index = (req, res) => {
-    const prs = [];
-    let userImage;
-
     function findPrs(username) {
         const _ = req.app.get('_');
         const q = req.app.get('q');
@@ -18,20 +15,17 @@ exports.index = (req, res) => {
             q: `-label:invalid+created:2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00+type:pr+is:public+author:${username}`
         };
 
-        github.search.issues(options, (err, res) => {
-            if (err) {
-                deferred.reject();
-                return;
-            }
+        q.all([
+            github.search.issues(options),
+            github.users.getForUser({ username })
+        ]).then(gitData => {
+            const foundPrs = gitData[0];
+            const user     = gitData[1];
 
-            userImage = null;
+            const prs = [];
 
-            _.each(res.data.items, event => {
+            _.each(foundPrs.data.items, event => {
                 const repo = event.pull_request.html_url.substring(0, event.pull_request.html_url.search('/pull'));
-
-                if (userImage == null) {
-                    userImage = event.user.avatar_url;
-                }
 
                 const hacktoberFestLabels = _.filter(event.labels, label => label.name.toLowerCase() === 'hacktoberfest');
 
@@ -45,8 +39,10 @@ exports.index = (req, res) => {
 
                 prs.push(returnedEvent);
             });
-
-            deferred.resolve();
+            
+            deferred.resolve({ prs, user });
+        }).catch(err => {
+            deferred.reject(err);
         });
 
         return deferred.promise;
@@ -57,8 +53,8 @@ exports.index = (req, res) => {
         return res.render('index');
     }
 
-    findPrs(req.query.username).then(() => {
-        let length = prs.length;
+    findPrs(req.query.username).then(data => {
+        let length = data.prs.length;
 
         const statements = [
             'It\'s not too late to start!',
@@ -72,17 +68,22 @@ exports.index = (req, res) => {
         if (length > 5) length = 5;
 
         if (req.xhr) {
-            console.log(prs);
-            res.render('partials/prs', {prs, statement: statements[length], userImage, layout: false});
+            res.render('partials/prs', {
+                prs: data.prs,
+                statement: statements[length],
+                userImage: data.user.data.avatar_url,
+                layout: false
+            });
         } else {
-            res.render('index', {
-                prs,
+            data.render('index', {
+                prs: data.prs,
                 statement: statements[length],
                 username: req.query.username,
-                userImage
+                userImage: data.user.data.avatar_url
             });
         }
-    }).catch(() => {
+    }).catch((e) => {
+        console.log(e);
         if (req.xhr) {
             res.render('partials/error', {layout: false});
         } else {
