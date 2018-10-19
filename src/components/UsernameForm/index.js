0c70e485e15081ad9ff76683cd5068dc8ef3e66c
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-10-20 01:01:48 +0200

    Reorganised pages, began working on username form

---
+++ src/components/UsernameForm/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UsernameForm = () => (
+  <h4>Username form</h4>
+);
+
+export default UsernameForm;
--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-const Home = () => (
-  <Fragment>
-    <h1>Home</h1>
-  </Fragment>
-);
-
-export default Home;
