297cef7647728386e6fe9ad173e7c6f72480bba8
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-10-20 01:03:43 +0200

    Finished TimeMessage

--- src/components/UsernameForm/index.js
+++ src/components/UsernameForm/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import TimeMessage from './TimeMessage';
 
 const UsernameForm = () => (
-  <h4>Username form</h4>
+  <div className="pb-4 md:pt-16">
+    <TimeMessage />
+    <h4>Username form</h4>
+  </div>
 );
 
 export default UsernameForm;
