43abec61aa94a2cac4b5aa8c8703c9f9ee7e2785
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-10-20 01:15:36 +0200

    Finished UsernameForm

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PageWrapper = ({ children, ...props }) => (
-  <div className="md:py-4 flex-grow flex-no-shrink">
+  <div className="md:py-4 flex-grow flex-no-shrink" {...props}>
     <div className="md:py-4">
       {children}
     </div>
