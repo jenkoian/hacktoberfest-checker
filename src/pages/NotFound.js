53f0e8294c9b9dd0711d8fec8820b2d32b1e8e0c
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-10-20 00:52:16 +0200

    Update not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,15 @@ const NotFound = () => (
     <Helmet>
       <title>Page not found</title>
     </Helmet>
-    <h1>404</h1>
+    <h2 className="text-5xl md:text-xxl text-center text-white py-20">Oops!</h2>
+    <p className="text-center py-4 text-white">
+      The page you are looking for does not exist.
+    </p>
+    <p className="text-center">
+      <a className="text-orange" href="/">
+        Return to homepage
+      </a>
+    </p>
   </Fragment>
 );
 
