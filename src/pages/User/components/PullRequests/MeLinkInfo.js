76e96945bfefbd354524ee698c95bea7d7e9ce40
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2019-10-01 23:20:46 +0700

    Dynamic Me Link (#362)
    * Dynamic Me Link
    
    * Add missing semi colons
    
    * Add missing semi colons

--- src/pages/User/components/PullRequests/MeLinkInfo.js
+++ src/pages/User/components/PullRequests/MeLinkInfo.js
@@ -6,17 +6,21 @@ export default class MeLinkInfo extends Component {
     username: PropTypes.string.isRequired
   };
 
-  storeUsernameAsMe = () =>
+  storeUsernameAsMe = () => {
     localStorage.setItem('myGithub', this.props.username);
+    this.forceUpdate();
+  };
 
-  render = () => (
-    <div className="rounded mx-auto mt-16 overflow-hidden w-5/6 lg:w-1/2 mt-4">
+  render = () => {
+    let storeUsernameBtn = (
       <button
         className="bg-mid-purple text-white hover:bg-light-blue hover:text-mid-blue mx-auto mt-2 h-8 border-none pointer rounded-sm px-4 block saveUser"
         onClick={this.storeUsernameAsMe}
       >
         This is Me
       </button>
+    );
+    let infoStr = (
       <p className="text-mid-grey mx-auto text-center my-4">
         In the future, you can find your PRs by visiting{' '}
         <a
@@ -29,6 +33,30 @@ export default class MeLinkInfo extends Component {
         </a>{' '}
         on this device.
       </p>
-    </div>
-  );
+    );
+    const savedUsername = localStorage.getItem('myGithub');
+    if (savedUsername === this.props.username) {
+      storeUsernameBtn = null;
+      infoStr = (
+        <p className="text-mid-grey mx-auto text-center my-4">
+          Username {this.props.username} saved! You can visit{' '}
+          <a
+            href={`${process.env.REACT_APP_HOSTNAME}/me`}
+            className="link text-mid-purple underline-hover saveUser"
+            id="melink"
+          >
+            {process.env.REACT_APP_HOSTNAME}
+            /me
+          </a>{' '}
+          now!
+        </p>
+      )
+    }
+    return (
+      <div className="rounded mx-auto mt-16 overflow-hidden w-5/6 lg:w-1/2 mt-4">
+        {storeUsernameBtn}
+        {infoStr}
+      </div>
+    )
+  };
 }
