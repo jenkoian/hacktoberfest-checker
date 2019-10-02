88bffc3e1477ac97807a3c11cd498c625d3fbcf5
Derek Willingham <derek.willingham@tinroofsoftware.com>
2019-10-02 17:48:09 -0400

    fix FaqEntry component

--- src/pages/Faq/components/FaqEntry.js
+++ src/pages/Faq/components/FaqEntry.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const FaqEntry = data => (
-  <div className="flex flex-wrap justify-center content-center">
-    <h2 className="text-orange">{data.question}</h2>
-    <p className="text-white">{data.answer}</p>
-  </div>
-);
+const FaqEntry = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="pb-2 flex flex-wrap justify-left content-center rounded mx-auto my-5 overflow-hidden w-5/6 lg:w-1/2 ">
+      <h2 className="text-orange">{data.question}</h2>
+      <p className="text-white">{data.answer}</p>
+    </div>
+  );
+};
 
-export default FaqEntry
+export default FaqEntry;
--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -8,7 +8,7 @@ const Faq = () => (
     <SiteTitle>Hacktoberfest Checker</SiteTitle>
     <h1 className="text-white text-center text-4xl font-extrabold">F.A.Q.</h1>
     {faqData.map(data => (
-        <FaqEntry data={data}></FaqEntry>
+      <FaqEntry key={data.question} data={data}></FaqEntry>
     ))}
   </Fragment>
 );
