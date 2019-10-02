beb9e9f80b81c40a5f8ea6ee936faffc54a3f45a
Axel Kriewel <axel@kriewel.de>
2019-10-02 15:35:25 +0200

    Added some hints; suggestion for Message when Event is over

--- src/components/UsernameForm/TimeMessage/getTimeMessage.js
+++ src/components/UsernameForm/TimeMessage/getTimeMessage.js
@@ -6,20 +6,26 @@ const getTimeMessage = () => {
   if (currentMonth !== 9) {
     return "It isn't even October yet!";
   }
+  
+  /*
+  if (daysLeft < 0) {
+    return "Not October anymore. It's all over now! To late to commit your pull requests";
+  }
+  */
 
   if (daysLeft === 0) {
     return "It's the very last day! Get your last PRs in!";
   }
 
   if (daysLeft === 1) {
-    return 'One more day, keep it going!';
+    return "One more day, keep it going!";
   }
 
   if (daysLeft < 10) {
-    return `There's only ${daysLeft} days left! You can do it!`;
+    return "There's only ${daysLeft} days left to make your pull requests (PRs)! You can do it!";
   }
 
-  return `There's ${daysLeft} days remaining!`;
+  return "There's ${daysLeft} days remaining to make your pull requests (PRs)!";
 };
 
 export default getTimeMessage;
