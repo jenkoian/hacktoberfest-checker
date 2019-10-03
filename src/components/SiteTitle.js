9d6c9c332b96598378c955916acc795a76c042af
Zac Davis <zac.davis@coxautoinc.com>
2019-10-03 13:16:24 -0400

    Add glow around Hacktober title

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SiteTitle = () => (
   <a className="no-underline text-center py-12 md:py-8" href="/">
-    <h1 className="text-blue-700 text-4xl md:text-6xl">
+    <h1 className="text-blue-700 text-4xl md:text-6xl text-glow">
       Hacktober<span className="text-mid-purple">fest</span>
     </h1>
     <small className="block text-yellow-400 text-base italic -mt-4 -mr-32 md:-mr-64">Checker</small>
