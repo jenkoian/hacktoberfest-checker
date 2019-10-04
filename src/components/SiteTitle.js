7751b7a4065827b680bac5ca27326c5b17de4d7c
echo webid <saya@echo.web.id>
2019-10-04 16:18:43 +0700

    Glowly Text

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -3,8 +3,9 @@ import React from 'react';
 const SiteTitle = () => (
   <div className="text-center">
     <a className="no-underline inline-block py-12 md:py-8" href="/">
-      <h1 className="text-blue-700 text-4xl md:text-6xl">
-        Hacktober<span className="text-mid-purple">fest</span>
+      <h1 className="text-blue-700 text-4xl md:text-6xl glowly-blue">
+        Hacktober
+        <span className="text-mid-purple">fest</span>
       </h1>
       <small className="block text-right -mt-4 mr-8 text-yellow text-base italic">
         Checker
