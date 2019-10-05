4b95356d7238dd9d32d088796a74642455ac6a38
octaviotastico <octaviopercivaldi2@gmail.com>
2019-10-05 03:14:53 -0300

    Solved issue #379 (Added Glowy Title)

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const SiteTitle = () => (
-  <div className="text-center">
-    <a className="no-underline inline-block py-12 md:py-8" href="/">
-      <h1 className="text-blue-700 text-4xl md:text-6xl">
+  <div className="text-center glowing-text">
+    <a className="no-underline inline-block py-12 md:py-8 glowing-text" href="/">
+      <h1 className="text-blue-700 text-4xl md:text-6xl glowing-text">
         Hacktober<span className="text-mid-purple">fest</span>
       </h1>
       <small className="block text-right -mt-4 mr-8 text-yellow text-base italic">
