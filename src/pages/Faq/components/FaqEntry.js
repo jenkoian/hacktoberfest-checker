c527c4a3bdc0db5c9cd226b1b767f58f2f8e220e
Deddy T <deddybuster@hotmail.com>
2019-10-08 00:23:50 +0800

    Changed h2 to h3 for faq questions

--- src/pages/Faq/components/FaqEntry.js
+++ src/pages/Faq/components/FaqEntry.js
@@ -4,7 +4,7 @@ const FaqEntry = ({ data }) => {
   return (
     <div className="faqs shadow-lg flex text-mid-grey mb-6 p-4 break-words">
       <div className="pb-2 flex flex-wrap justify-left content-center rounded mx-auto my-5 overflow-hidden w-5/6 lg:w-100 ">
-        <h2 className="text-light-blue mb-4">{data.question}</h2>
+        <h3 className="text-light-blue mb-4">{data.question}</h3>
         <p className="text-mid-grey">{data.answer}</p>
       </div>
     </div>
