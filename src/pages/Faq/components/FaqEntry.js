5120da03570235a665552c245754c03180ed2c50
Deddy T <deddybuster@hotmail.com>
2019-10-08 16:24:43 +0800

    Added FAQs Page with Styles (#388)
    * add FAQ page with bugs
    
    * fix FaqEntry component
    
    * 1. Added FAQs button on homepage with consistent button styling
    2. FAQs page with design similar to pull request design for consistency
    
    * Removed unnecessary code
    
    * Removed text orange styling
    
    * Deleted console log comment
    
    * Changed h2 to h3 for faq questions
    
    * Update faq title to be h2
    
    * Added new line at the end

---
+++ src/pages/Faq/components/FaqEntry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FaqEntry = ({ data }) => {
+  return (
+    <div className="faqs shadow-lg flex text-mid-grey mb-6 p-4 break-words">
+      <div className="pb-2 flex flex-wrap justify-left content-center rounded mx-auto my-5 overflow-hidden w-5/6 lg:w-100 ">
+        <h3 className="text-light-blue mb-4">{data.question}</h3>
+        <p className="text-mid-grey">{data.answer}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqEntry;
