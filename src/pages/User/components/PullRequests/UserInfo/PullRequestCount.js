d499ad675a9a169148ef867a2e58bd88317e6ab2
Chris Galvan <chrisgalvanr@gmail.com>
2019-10-09 21:06:00 -0500

    feat: center the text

--- src/pages/User/components/PullRequests/UserInfo/PullRequestCount.js
+++ src/pages/User/components/PullRequests/UserInfo/PullRequestCount.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import pullRequestAmount from '../pullRequestAmount';
 
 const PullRequestCount = ({ pullRequestCount }) => (
-  <span className="block text-5xl font-medium w-64">
+  <span className="block text-5xl font-medium w-64 self-center">
     <span className="text-yellow">{pullRequestCount}</span> /{' '}
     {pullRequestAmount}
   </span>
