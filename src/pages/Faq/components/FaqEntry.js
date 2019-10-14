d7c9d7dd0b549b2090c726720babe867b09db9be
Andrew <andrew.taylor@boxuk.com>
2019-10-14 10:32:59 +0100

    Fixed FAQ text overflow issue (#394)
    On the FAQ page, when viewing the page on mobile, text was flowing outside of its container. This commit resolves this issue.

--- src/pages/Faq/components/FaqEntry.js
+++ src/pages/Faq/components/FaqEntry.js
@@ -5,7 +5,7 @@ const FaqEntry = ({ data }) => {
     <div className="faqs shadow-lg flex text-mid-grey mb-6 p-4 break-words">
       <div className="pb-2 flex flex-wrap justify-left content-center rounded mx-auto my-5 overflow-hidden w-5/6 lg:w-100 ">
         <h3 className="text-light-blue mb-4">{data.question}</h3>
-        <p className="text-mid-grey">{data.answer}</p>
+        <p className="text-mid-grey overflow-auto">{data.answer}</p>
       </div>
     </div>
   );
