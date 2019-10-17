028bb68bfefc61d94ae88ce4e9280b8f98a4761d
Bimochan Shrestha <bmochan@gmail.com>
2019-10-17 12:31:37 +0545

    Change username to user in URL
    In social share buttons, Since our profile page URL has /user/{username}, it needs to be changed as well.

--- src/pages/User/components/PullRequests/ShareButtons.js
+++ src/pages/User/components/PullRequests/ShareButtons.js
@@ -10,7 +10,7 @@ const ShareButtons = ({ username, pullRequestCount }) => (
         target="_blank"
         rel="noopener noreferrer"
         className="bg-blue text-white rounded px-2 py-1 pointer text-white no-underline text-sm"
-        href={`https://twitter.com/intent/tweet?text=My progress on frogtoberfest ${pullRequestCount} / ${pullRequestAmount}&url=${HOSTNAME}/username/${username}&hashtags=frogtoberfest, frogtoberfest`}
+        href={`https://twitter.com/intent/tweet?text=My progress on frogtoberfest ${pullRequestCount} / ${pullRequestAmount}&url=${HOSTNAME}/user/${username}&hashtags=frogtoberfest, frogtoberfest`}
         data-size="large"
       >
         <i className="fab fa-twitter fa-lg" /> Tweet
@@ -27,7 +27,7 @@ const ShareButtons = ({ username, pullRequestCount }) => (
         target="_blank"
         rel="noopener noreferrer"
         className="fb-xfbml-parse-ignore bg-blue-dark text-white rounded px-2 py-1 pointer text-white no-underline text-sm"
-        href={`https://www.facebook.com/sharer/sharer.php?u=${HOSTNAME}/username/${username}`}
+        href={`https://www.facebook.com/sharer/sharer.php?u=${HOSTNAME}/user/${username}`}
       >
         <i className="fab fa-facebook fa-lg" /> Share
       </a>
