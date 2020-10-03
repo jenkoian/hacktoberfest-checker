c6b98fd41edf70bbeec65bf6caee7060523a1d16
Ryan Willpower <30806319+Ryan-Willpower@users.noreply.github.com>
2020-10-03 18:16:57 +0700

    feat: add prettier husky hooks (#432)

--- src/pages/User/components/PullRequests/LoadingIcon/index.js
+++ src/pages/User/components/PullRequests/LoadingIcon/index.js
@@ -3,7 +3,13 @@ import loadingIcon from './loading-icon.gif';
 
 const LoadingIcon = () => (
   <div className="text-center rounded-full overflow-hidden">
-    <img className="inline rounded-full object-cover" src={loadingIcon} width="24" height="24" alt="Loading icon" />
+    <img
+      className="inline rounded-full object-cover"
+      src={loadingIcon}
+      width="24"
+      height="24"
+      alt="Loading icon"
+    />
   </div>
 );
 
--- src/pages/User/components/PullRequests/UserInfo/UserImage.js
+++ src/pages/User/components/PullRequests/UserInfo/UserImage.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class UserImage extends Component {
-
   constructor(props) {
     super(props);
 
     this.divStyle = {
-      backgroundImage: 'url(' + this.props.userImage + ')',
+      backgroundImage: 'url(' + this.props.userImage + ')'
     };
   }
 
   render = () => (
     <div className="h-24 w-24 relative my-4 mx-0 border-solid border-4 border-white-600">
-      <img src={ this.props.userImage } alt={ this.props.username } />
+      <img src={this.props.userImage} alt={this.props.username} />
     </div>
   );
 }
