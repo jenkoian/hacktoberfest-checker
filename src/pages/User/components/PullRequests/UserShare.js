c8d22a19fc9bac5034829a513e9d14c99110e78a
Farelion <mczownik@gmail.com>
2020-10-07 16:27:24 +0200

    Rewrite to function method

--- src/pages/User/components/PullRequests/UserShare.js
+++ src/pages/User/components/PullRequests/UserShare.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class UserShare extends Component {
-  render = () => {
-    window.twttr = (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return t;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://platform.twitter.com/widgets.js';
-      fjs.parentNode.insertBefore(js, fjs);
+const UserShare = () => {
 
-      t._e = [];
-      t.ready = function (f) {
-        t._e.push(f);
-      };
-      return t;
-    })(document, 'script', 'twitter-wjs');
+  return <React.Fragment>
+    <Twitter/>,
+    <FbRoot/>,
+    <Facebook/>
+  </React.Fragment>
+};
 
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
 
-    return <div id="fb-root"></div>;
-  };
+const FbRoot = () => { 
+  return <div id="fb-root"></div>;
 }
+
+const Twitter = () => {
+   window.twttr = (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function (f) {
+      t._e.push(f);
+    };
+    return t;
+  })(document, 'script', 'twitter-wjs');
+  return null;
+};
+
+const Facebook = () => {
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10';
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, 'script', 'facebook-jssdk');
+  return null;
+};
+
+
+export default UserShare;
