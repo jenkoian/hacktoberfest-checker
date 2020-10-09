7cc1f86a8bf2136b9edfbe5b1eba26a92e99a3a6
palak001 <palak7372@gmail.com>
2020-10-09 17:01:49 +0530

    Used fontawesome for info icon at top of page

--- src/components/RegisterReminder.js
+++ src/components/RegisterReminder.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle as infoIcon } from '@fortawesome/free-solid-svg-icons';
 
 const RegisterReminder = () => (
   <div className="flex flex-none items-center justify-center p-2 pl-4 bg-dark-blue-alt light-mode:bg-light-blue text-mid-grey light-mode:text-dark-grey">
-    <svg
-      className="w-4 h-4"
-      data-icon="info"
-      viewBox="0 0 32 32"
-      style={{ fill: 'currentcolor' }}
-    >
-      <title>info</title>
-      <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6" />
-    </svg>
+    <FontAwesomeIcon icon={infoIcon} size={'m'} title="info" />
+
     <span className="text-md leading-tight ml-4 mr-16 md:mr-8">
       Remember to{' '}
       <a
