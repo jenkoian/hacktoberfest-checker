bd19e9e0a3b026258513129a6e60fb29dbe1f4d0
Balbaal <iqbal.mi20@gmail.com>
2020-10-18 00:16:58 +0800

    fix: roolback to old logo

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const SiteTitle = () => (
-  <div className="text-center hero-title">
+  <div className="text-center">
     <a className="no-underline inline-block py-12 md:py-8" href="/">
-      <img src="https://hacktoberfest.digitalocean.com/assets/HF-full-logo-b05d5eb32b3f3ecc9b2240526104cf4da3187b8b61963dd9042fdc2536e4a76c.svg" />
-      <small className="block text-light-blue light-mode:text-dark-blue uppercase block text-3xl md:text-4xl">
+      <h1 className="text-dark-pink text-6xl md:text-7xl">
+        Hacktober
+        <span className="text-light-pink text-3xl md:text-5xl uppercase block">
+          fest
+        </span>
+      </h1>
+      <small className="block text-light-blue light-mode:text-dark-blue uppercase block">
         Checker
       </small>
     </a>
