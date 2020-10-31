ee5fc4d233349e9c51221795ad01d7141da420d7
MihirGH <mihir.shah@sprinklr.com>
2020-10-31 14:26:20 +0530

    chore: Refactor PullRequests class component to function component

--- src/pages/User/components/PullRequests/index.js
+++ src/pages/User/components/PullRequests/index.js
@@ -1,5 +1,11 @@
-import React, { Component, Fragment } from 'react';
+// Libraries
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+// Hooks
+import useFetchPullRequests from './hooks/useFetchPullRequests';
+
+// Components
 import LoadingIcon from './LoadingIcon';
 import ErrorText from './ErrorText';
 import pullRequestAmount from './pullRequestAmount';
@@ -9,113 +15,51 @@ import PullRequest from './PullRequest';
 import IssuesLink from './IssuesLink';
 import MeLinkInfo from './MeLinkInfo';
 
-export default class PullRequests extends Component {
-  static defaultProps = {
-    username: PropTypes.string.isRequired,
-  };
+export default function PullRequests({ username }) {
+  const { loading, data, error } = useFetchPullRequests(username);
 
-  state = {
-    loading: true,
-    data: null,
-    error: null,
-  };
-
-  componentDidMount = () => {
-    this.storeUsernameAsMe();
-    this.fetchPullRequests();
-  };
-
-  componentDidUpdate = (prevProps) => {
-    if (prevProps.username === this.props.username) return;
-    this.fetchPullRequests();
-  };
-
-  storeUsernameAsMe = () => {
-    const username = this.props.username;
-
-    if (localStorage.getItem('myGithub')) {
-      return;
-    }
-
+  useEffect(() => {
+    if (localStorage.getItem('myGithub')) return;
     localStorage.setItem('myGithub', username);
-  };
+  }, []);
 
-  fetchPullRequests = () => {
-    const username = this.props.username;
-    const apiUrl = process.env.REACT_APP_API_URL;
+  const getErrorMessage = () =>
+    error?.error_description ??
+    data?.error_description ??
+    "Couldn't find any data or we hit an error, err try again?";
 
-    this.setState({
-      loading: true,
-    });
+  if (loading) {
+    return <LoadingIcon />;
+  }
 
-    fetch(`${apiUrl}/prs?username=${username}`, {
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      .then((pullRequests) =>
-        this.setState({
-          loading: false,
-          data: pullRequests,
-        })
-      )
-      .catch((error) =>
-        this.setState({
-          loading: false,
-          error,
-        })
-      );
-  };
+  if (error || data.error_description) {
+    return <ErrorText errorMessage={getErrorMessage()} />;
+  }
 
-  getErrorMessage = () => {
-    const { data, error } = this.state;
+  const isComplete = data.prs.length >= pullRequestAmount;
 
-    if (error && error.description) {
-      return error.error_description;
-    }
-
-    if (data && data.error_description) {
-      return data.error_description;
-    }
-
-    return "Couldn't find any data or we hit an error, err try again?";
-  };
-
-  render = () => {
-    const username = this.props.username;
-    const { loading, data, error } = this.state;
-
-    if (loading) {
-      return <LoadingIcon />;
-    }
-
-    if (error || data.error_description) {
-      return <ErrorText errorMessage={this.getErrorMessage()} />;
-    }
-
-    const isComplete = data.prs.length >= pullRequestAmount;
-
-    return (
-      <Fragment>
-        <div className="text-center text-white">
-          <ShareButtons
-            username={username}
-            pullRequestCount={data.prs.length}
-          />
-          <UserInfo
-            username={username}
-            userImage={data.userImage}
-            pullRequestCount={data.prs.length}
-          />
-        </div>
-        <div className="mx-auto w-5/6 lg:w-1/2 mb-4">
-          {data.prs.length > 0 &&
-            data.prs.map((pullRequest, i) => (
-              <PullRequest pullRequest={pullRequest} key={i} />
-            ))}
-        </div>
-        {!isComplete && <IssuesLink />}
-        <MeLinkInfo username={username} />
-      </Fragment>
-    );
-  };
+  return (
+    <Fragment>
+      <div className="text-center text-white">
+        <ShareButtons username={username} pullRequestCount={data.prs.length} />
+        <UserInfo
+          username={username}
+          userImage={data.userImage}
+          pullRequestCount={data.prs.length}
+        />
+      </div>
+      <div className="mx-auto w-5/6 lg:w-1/2 mb-4">
+        {data.prs.length > 0 &&
+          data.prs.map((pullRequest, i) => (
+            <PullRequest pullRequest={pullRequest} key={i} />
+          ))}
+      </div>
+      {!isComplete && <IssuesLink />}
+      <MeLinkInfo username={username} />
+    </Fragment>
+  );
 }
+
+PullRequests.defaultProps = {
+  username: PropTypes.string.isRequired,
+};
