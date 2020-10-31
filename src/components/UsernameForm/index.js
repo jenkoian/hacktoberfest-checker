d00cbbabb00d87edc550d776a42da8a72e2be0b4
MihirGH <mihir.shah@sprinklr.com>
2020-10-31 20:13:17 +0530

    chore: Refactor UsernameForm component to function component

--- src/components/UsernameForm/index.js
+++ src/components/UsernameForm/index.js
@@ -1,61 +1,58 @@
-import React, { Component } from 'react';
+// Libraries
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
+// Components
 import TimeMessage from './TimeMessage';
 import UsernameInput from './UsernameInput';
 import CheckButton from './CheckButton';
 
-class UsernameForm extends Component {
-  static propTypes = {
-    username: PropTypes.string,
-    // Provided by withRouter()
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-  };
+const getUserUrl = (username) => `/user/${username}`;
 
-  static defaultProps = {
-    username: '',
-  };
+export default function UsernameForm(props) {
+  const [username, setUsername] = useState(props.username);
+  const history = useHistory();
 
-  state = {
-    username: this.props.username,
-  };
+  const handleUsernameChange = useCallback(
+    (e) => setUsername(e.target.value),
+    []
+  );
 
-  handleUsernameChange = (e) => this.setState({ username: e.target.value });
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+      if (username.trim().length === 0) {
+        return;
+      }
 
-    const username = this.state.username;
+      const userUrl = getUserUrl(username);
+      history.push(userUrl);
+    },
+    [username, history]
+  );
 
-    if (username.trim().length === 0) {
-      return;
-    }
-
-    const userUrl = this.getUserUrl(username);
-    this.props.history.push(userUrl);
-  };
-
-  getUserUrl = (username) => `/user/${username}`;
-
-  render = () => (
+  return (
     <div className="pb-4 md:pt-16">
       <TimeMessage />
       <form
         action="/"
         className="flex h-12 mx-auto w-5/6 md:w-3/5 lg:w-1/3"
         method="get"
-        onSubmit={this.handleSubmit}
+        onSubmit={handleSubmit}
       >
-        <UsernameInput
-          value={this.state.username}
-          onChange={this.handleUsernameChange}
-        />
+        <UsernameInput value={username} onChange={handleUsernameChange} />
         <CheckButton />
       </form>
     </div>
   );
 }
 
-export default withRouter(UsernameForm);
+UsernameForm.propTypes = {
+  username: PropTypes.string,
+};
+
+UsernameForm.defaultProps = {
+  username: '',
+};
