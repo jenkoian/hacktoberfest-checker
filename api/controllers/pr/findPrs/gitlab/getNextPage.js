9989ebb96282b463f17899e9f079814bec3e34c1
Greg Sithole <gregsithole@hotmail.co.za>
2021-10-02 16:20:19 +0200

    Changed functions to Async-Await in getNextPage
    This applies for both Github & Gitlab

--- api/controllers/pr/findPrs/gitlab/getNextPage.js
+++ api/controllers/pr/findPrs/gitlab/getNextPage.js
@@ -2,48 +2,35 @@
 
 const hasNextPage = require('./hasNextPage');
 
-const getNextPage = (
-  pagination,
-  gitlab,
-  username,
-  searchYear,
-  pullRequestData
-) =>
-  new Promise((resolve, reject) => {
-    gitlab.MergeRequests.all({
-      scope: 'all',
-      author_username: username,
-      //created_after: `${searchYear}-08-30T00:00:00-12:00`,
-      //created_before: `${searchYear}-10-31T23:59:59-12:00`,
-      // 30 is the default but this makes it clearer/allows it to be tweaked
-      per_page: pagination.perPage,
-      page: pagination.next,
-      showExpanded: true,
-    })
-      .then((res) => {
-        const newPullRequestData = pullRequestData.concat(res.data);
-        const pagination = res.paginationInfo;
+const getNextPage = async (pagination, gitlab, username, searchYear, pullRequestData) => {
+ try {
+   const mergeRequestResults = await gitlab.MergeRequests.all({
+     scope: 'all',
+     author_username: username,
+     //created_after: `${searchYear}-08-30T00:00:00-12:00`,
+     //created_before: `${searchYear}-10-31T23:59:59-12:00`,
+     // 30 is the default but this makes it clearer/allows it to be tweaked
+     per_page: pagination.perPage,
+     page: pagination.next,
+     showExpanded: true,
+   });
 
-        if (hasNextPage(pagination)) {
-          getNextPage(
-            pagination,
-            gitlab,
-            username,
-            searchYear,
-            newPullRequestData
-          ).then((pullRequestData) => resolve(pullRequestData));
-          return;
-        }
+   const newPullRequestData = pullRequestData.concat(mergeRequestResults.data);
+   const pagination = mergeRequestResults.paginationInfo;
 
-        if (process.env.NODE_ENV !== 'production') {
-          console.log(`Found ${pullRequestData.length} pull requests.`);
-        }
-        resolve(newPullRequestData);
-      })
-      .catch((err) => {
-        console.log('Error: ' + err);
-        return reject();
-      });
-  });
+   if (hasNextPage(pagination)) {
+     return await getNextPage(pagination, gitlab, username, searchYear, newPullRequestData);
+   }
+
+   if (process.env.NODE_ENV !== 'production') {
+     console.log(`Found ${pullRequestData.length} pull requests.`);
+   }
+
+   return pullRequestData;
+ } catch (error) {
+   console.log('Error: ' + error);
+   return error;
+ }
+}
 
 module.exports = getNextPage;
