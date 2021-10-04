de41af90184c9778882079377e425113a3de043a
vincentrohde <vincent.rohde@icloud.com>
2021-10-05 00:52:56 +0200

    set type for input of logCallsRemaining

--- api/controllers/pr/logCallsRemaining.ts
+++ api/controllers/pr/logCallsRemaining.ts
@@ -1,5 +1,9 @@
-const logCallsRemaining = (res: any) => {
-  const callsRemaining = res.headers['x-ratelimit-remaining'];
+import { OctokitResponse } from '@octokit/types/dist-types';
+
+const logCallsRemaining = (res: OctokitResponse<any, number>) => {
+  const callsRemaining = (res.headers[
+    'x-ratelimit-remaining'
+  ] as unknown) as number;
 
   if (process.env.NODE_ENV !== 'production' || callsRemaining < 100) {
     console.log(`API calls remaining: ${callsRemaining}`);
