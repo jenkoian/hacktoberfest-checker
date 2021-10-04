07918fcdd92807b45a348f5e9d88ff5e9df18360
odlave <evaldo.felipe2@gmail.com>
2021-10-04 18:13:11 -0300

    minor: some changes in 2021 Hacktoberfest theme;

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -4,12 +4,12 @@ const SiteTitle = () => (
   <div className="text-center">
     <a className="no-underline inline-block py-12 md:py-8" href="/">
       <h1 className="text-hack-logo text-6xl md:text-7xl">
-        Hacktober
-        <span className="text-hack-alt-logo text-3xl md:text-5xl uppercase block">
+        hacktober
+        <span className="block text-hack-alt-logo text-3xl md:text-5xl">
           fest
         </span>
       </h1>
-      <small className="block text-hack-alt-fg light-mode:text-hack-dark-title uppercase block">
+      <small className="block text-hack-alt-fg light-mode:text-hack-dark-title uppercase">
         Checker
       </small>
     </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       },
       colors: {
         'hack-dark-bg': '#2B3531',
+        'hack-light-bg': '#F4F0E1',
         'hack-alt-bg': '#677662',
         'hack-fg': '#F0FDF4',
         'hack-alt-fg': '#91A88C',
@@ -13,23 +14,7 @@ module.exports = {
         'hack-dark-title': '#2B3531',
         'hack-logo': '#F74700',
         'hack-alt-logo': '#B53A25',
-        'hack-mid-fg': '#A88771',
-
-        // 'dark-blue': '#072540',
-        // 'dark-blue-alt': '#183d5d',
-        // 'light-blue': '#93c2db',
-        // 'mid-blue': '#0069ff',
-        // 'mid-purple': '#A11EC6',
-        // 'mid-grey': '#99a1b3',
-        // yellow: '#fff922',
-        // 'dark-pink': '#9c4668',
-        // 'light-pink': '#ff8ae2',
-
-        // Light mode additions
-        // 'white-grey': '#f5f5f5',
-        // 'light-grey': '#e3e4e4',
-        // 'soft-blue': '#7c99a9',
-        // 'dark-grey': '#333333',
+        'hack-mid-fg': '#A88771'
       },
       fontSize: {
         '7xl': '5rem',
