d1d310bde8bb1eeb54277b644f23c5b55a71f359
Hilary Matusiak <hilary.matusiak@gmail.com>
2021-10-06 08:25:03 -0400

    add: dark and light icons based on scheme

--- src/pages/User/components/PullRequests/PullRequest/PlatformIcon.js
+++ src/pages/User/components/PullRequests/PullRequest/PlatformIcon.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { useColorScheme } from 'use-color-scheme';
 import PropTypes from 'prop-types';
-import githubLogo from './github-logo.png';
-import gitlabLogo from './gitlab-logo.png';
+
+import githubLogoLight from './github-logo-light.png';
+import gitlabLogoLight from './gitlab-logo-light.png';
+import githubLogoDark from './github-logo-dark.png';
+import gitlabLogoDark from './gitlab-logo-dark.png';
 
 const PlatformIcon = ({ isGitLab }) => {
-  const resolvedIcon = isGitLab ? gitlabLogo : githubLogo;
+  const { scheme } = useColorScheme();
+  const lightScheme = scheme === 'light';
+  const caseVariable = lightScheme + '-' + isGitLab;
+  let displayIcon;
+  switch (caseVariable) {
+    case 'true-true':
+      displayIcon = gitlabLogoDark;
+      break;
+    case 'true-false':
+      displayIcon = githubLogoDark;
+      break;
+    case 'false-true':
+      displayIcon = gitlabLogoLight;
+      break;
+    case 'false-false':
+      displayIcon = githubLogoLight;
+      break;
+  }
   return (
     <div>
       <img
         className="inline rounded-full object-cover"
-        src={resolvedIcon}
+        src={displayIcon}
         width="24"
         height="24"
         alt="Loading icon"
