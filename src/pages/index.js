1993ae0ae32852a335e8ba4252490764f2aa4c45
DheekshaGopika <dheekshagopikas@gmail.com>
2021-10-10 16:38:55 +0000

    Refined imports in components and pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import User from "./User";
+import Me from "./Me";
+import NotFound from "./NotFound";
+import Faq from "./Faq";
+
+export {
+    Home,
+    User,
+    Me,
+    NotFound,
+    Faq
+}
