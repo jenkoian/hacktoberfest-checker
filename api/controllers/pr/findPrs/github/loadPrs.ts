4ac01efbf25a0b1b984bad7fe1f9f9c114f3d19f
vincentrohde <vincent.rohde@icloud.com>
2021-10-12 21:25:49 +0200

    #567 - add typings for /api/controllers/pr/findPrs/github/loadPrs.ts

--- api/controllers/pr/findPrs/github/loadPrs.ts
+++ api/controllers/pr/findPrs/github/loadPrs.ts
@@ -1,10 +1,17 @@
 import hasNextPage from './hasNextPage';
 import getNextPage from './getNextPage';
+import { Octokit } from '@octokit/rest';
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types';
 
 const buildQuery = (username: string, searchYear: number) =>
   `+created:${searchYear}-09-30T00:00:00-12:00..${searchYear}-10-31T23:59:59-12:00+type:pr+is:public+draft:false+author:${username}`;
 
-const loadPrs = async (github: any, username: string) => {
+const loadPrs = async (
+  github: Octokit,
+  username: string
+): Promise<
+  RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items']
+> => {
   try {
     const today = new Date();
     const currentMonth = today.getMonth();
@@ -12,11 +19,13 @@ const loadPrs = async (github: any, username: string) => {
     const searchYear = currentMonth < 9 ? currentYear - 1 : currentYear;
     const perPage = 100;
 
-    const githubPullRequest = await github.search.issuesAndPullRequests({
-      q: buildQuery(username, searchYear),
-      // 30 is the default but this makes it clearer/allows it to be tweaked
-      per_page: perPage,
-    });
+    const githubPullRequest: RestEndpointMethodTypes['search']['issuesAndPullRequests']['response'] = await github.search.issuesAndPullRequests(
+      {
+        q: buildQuery(username, searchYear),
+        // 30 is the default but this makes it clearer/allows it to be tweaked
+        per_page: perPage,
+      }
+    );
     if (githubPullRequest) {
       const pullRequestData = githubPullRequest.data.items;
       if (hasNextPage(githubPullRequest)) {
@@ -31,7 +40,7 @@ const loadPrs = async (github: any, username: string) => {
 
       return pullRequestData;
     }
-  } catch (error) {
+  } catch (error: unknown) {
     console.log('Error: ' + Error);
     return null;
   }
