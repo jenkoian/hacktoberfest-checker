f7cb6d8a1f15ecef21d978d236ee041eda3b934e
vincentrohde <vincent.rohde@icloud.com>
2021-10-15 14:03:35 +0200

    add typings for /api/controllers/pr/findPrs/github/getNextPage.ts

--- api/controllers/pr/findPrs/github/getNextPage.ts
+++ api/controllers/pr/findPrs/github/getNextPage.ts
@@ -1,14 +1,22 @@
+import { Octokit } from '@octokit/rest';
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types';
 import getPageLinks from './getPageLinks';
 import hasNextPage from './hasNextPage';
 
-const getNextPage = async (response, github, pullRequestData) => {
+const getNextPage = async (
+  response: RestEndpointMethodTypes['search']['issuesAndPullRequests']['response'],
+  github: Octokit,
+  pullRequestData: RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items']
+) => {
   try {
     const baseUrl = process.env.GITHUB_API_BASE_URL
       ? process.env.GITHUB_API_BASE_URL
       : 'https://api.github.com';
     const nextPageLink = await getPageLinks(response).next.replace(baseUrl, '');
 
-    const githubResults = await github.request('GET ' + nextPageLink);
+    const githubResults = (await github.request(
+      'GET ' + nextPageLink
+    )) as RestEndpointMethodTypes['search']['issuesAndPullRequests']['response'];
     const newPullRequestData = pullRequestData.concat(githubResults.data.items);
     if (hasNextPage(githubResults)) {
       return await getNextPage(githubResults, github, newPullRequestData);
@@ -18,7 +26,7 @@ const getNextPage = async (response, github, pullRequestData) => {
       console.log(`Found ${pullRequestData.length} pull requests.`);
     }
     return newPullRequestData;
-  } catch (error) {
+  } catch (error: unknown) {
     console.log('Error: ' + error);
     return error;
   }
