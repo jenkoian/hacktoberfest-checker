9a4782fb7acab6033ff559ff30da1ac550ba1386
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2021-10-21 09:54:04 +0300

    feat: Show usernames of friends page. Link to username page. Highlight me

--- src/pages/User/components/PullRequests/UserInfo/index.js
+++ src/pages/User/components/PullRequests/UserInfo/index.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import UserImage from './UserImage';
 import PullRequestCount from './PullRequestCount';
 import ResultMessage from './ResultMessage';
+import { MeContext } from 'context/Me';
 
-const UserInfo = ({ username, userImage, pullRequestCount }) => (
-  <div className="mb-6 flex flex-wrap justify-center content-center rounded mx-auto overflow-hidden w-5/6 lg:w-1/2">
-    <div className="mx-4">
-      {userImage && <UserImage userImage={userImage} username={username} />}
-    </div>
-    <div className="flex flex-wrap justify-center content-center flex-col">
-      <PullRequestCount pullRequestCount={pullRequestCount} />
-      <ResultMessage pullRequestCount={pullRequestCount} />
-    </div>
-  </div>
-);
+const UserInfo = ({
+  username,
+  userImage,
+  pullRequestCount,
+  showUsernameLink = false,
+}) => {
+  const { me } = useContext(MeContext);
+
+  return (
+    <>
+      {showUsernameLink && (
+        <div className="mt-8 text-hack-logo">
+          {username === me && (
+            <span
+              className="text-2xl inline-block mr-2 -mt-2 align-middle"
+              title="This is me"
+            >
+              ★
+            </span>
+          )}
+          <a href={`/user/${username}`}>{username}</a>
+        </div>
+      )}
+      <div className="mb-6 flex flex-wrap justify-center content-center rounded mx-auto overflow-hidden w-5/6 lg:w-1/2">
+        <div className="mx-4">
+          {userImage && <UserImage userImage={userImage} username={username} />}
+        </div>
+        <div className="flex flex-wrap justify-center content-center flex-col">
+          <PullRequestCount pullRequestCount={pullRequestCount} />
+          <ResultMessage pullRequestCount={pullRequestCount} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 UserInfo.propTypes = {
   username: PropTypes.string.isRequired,
   userImage: PropTypes.string.isRequired,
   pullRequestCount: PropTypes.number.isRequired,
+  showUsernameLink: PropTypes.bool,
 };
 
 export default UserInfo;
