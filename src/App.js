be190e26e2fd02687e81c5474f0c6fe7fbf13a55
Aarai Myers <aaraimyers@gmail.com>
2021-10-23 17:24:35 -0400

    Make FAQ link conditional

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => (
         </Switch>
       </Router>
     </PageWrapper>
-    <Footer />
+    <Footer path="/faq" />
   </>
 );
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,37 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestion as questionIcon } from '@fortawesome/free-solid-svg-icons';
 
-const Footer = () => (
-  <footer className="text-sm px-8 text-center flex-none py-4">
-    <p className="text-hack-fg light-mode:text-hack-dark-title">
-      Disclaimer: This site is fan made and not affiliated with{' '}
-      <a
-        href="https://hacktoberfest.digitalocean.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Hacktoberfest
-      </a>
-      .
-    </p>
-    <div className="p-2 my-4" id="faqs">
-      <a
-        rel="noopener noreferrer"
-        className="bg-hack-alt-bg hover:bg-hack-alt-fg text-hack-fg transition duration-300 hover:text-hack-fg px-2 py-1 pointer no-underline text-sm"
-        href="/Faq"
-      >
-        <FontAwesomeIcon icon={questionIcon} /> FAQs
-      </a>
-    </div>
-  </footer>
-);
+const Footer = ({ path }) => {
+  let url = window.location.href.toLocaleLowerCase();
+  let hideFaqLink = url.includes(path);
+  console.log(hideFaqLink);
+
+  return (
+    <footer className="text-sm px-8 text-center flex-none py-4">
+      <p className="text-hack-fg light-mode:text-hack-dark-title">
+        Disclaimer: This site is fan made and not affiliated with{' '}
+        <a
+          href="https://hacktoberfest.digitalocean.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hacktoberfest
+        </a>
+        .
+      </p>
+      <div className="p-2 my-4" id="faqs">
+        {!hideFaqLink && (
+          <a
+            rel="noopener noreferrer"
+            className="bg-hack-alt-bg hover:bg-hack-alt-fg text-hack-fg transition duration-300 hover:text-hack-fg px-2 py-1 pointer no-underline text-sm"
+            href="/Faq"
+          >
+            <FontAwesomeIcon icon={questionIcon} /> FAQs
+          </a>
+        )}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
