50911c21b68c9ae11b71bd98eadc687797642c99
Deepak Kamath K <dkamath1998@gmail.com>
2022-10-19 01:32:11 +0530

    Fix : Invisible word - issues was missing the className field

--- src/pages/User/components/PullRequests/IssuesLink.js
+++ src/pages/User/components/PullRequests/IssuesLink.js
@@ -4,7 +4,8 @@ const IssuesLink = () => (
   <div className="flex flex-wrap justify-center content-center text-center">
     <div className="text-hack-fg light-mode:text-hack-dark-title pb-4">
       Look at the following{' '}
-      <a className="light-mode:text-hack-dark-title light-mode:hover:text-hack-dark-title"
+      <a
+        className="light-mode:text-hack-dark-title light-mode:hover:text-hack-dark-title" 
         href="https://github.com/search?q=label:hacktoberfest+state:open+type:issue"
         target="_blank"
         rel="noopener noreferrer"
